4a8ca34dd33bec91d174130e5b56f0a3cbd5cef2
alexoseg <aoseg@stanford.edu>
2020-07-01 12:31:21 -0700

    API endpoint now fetches full Twitter Text

--- twitter/API/APIManager.m
+++ twitter/API/APIManager.m
@@ -50,7 +50,7 @@ static NSString * const consumerSecret = @"urOWycaJThsMxfkLCbPW6P5kcK0EDNcKXLZO6
 
 - (void)getHomeTimelineWithCompletion:(void(^)(NSArray *tweets, NSError *error))completion {
     
-    [self GET:@"1.1/statuses/home_timeline.json"
+    [self GET:@"1.1/statuses/home_timeline.json?tweet_mode=extended"
    parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, NSArray *  _Nullable tweetDictionaries) {
 
        // Manually cache the tweets. If the request fails, restore from cache if possible.
